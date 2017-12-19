6618ab57a1793ec5c4cb33cdcda246a6ff5bc865
Per Steenstrup <per.steenstrup@gmail.com>
2017-12-18 17:56:54 -0800

    Per API notes

--- apps/main_app/views.py
+++ apps/main_app/views.py
@@ -8,6 +8,21 @@ from datetime import datetime
 import random
 
 # Create your views here.
+'''
+Notes on APIs:
+World Bank API call for % of population that are dependent minors
+http://api.worldbank.org/v2/countries/all/indicators/SP.POP.DPND.YG?date=2016:2016 XML format
+http://api.worldbank.org/v2/countries/all/indicators/SP.POP.DPND.YG?format=json JSON format <- Messy gives all the years
+
+https://datahelpdesk.worldbank.org/knowledgebase/articles/898581-api-basic-call-structure
+https://datahelpdesk.worldbank.org/knowledgebase/articles/898599-api-indicator-queries
+
+https://maps.googleapis.com/maps/api/directions/json?origin=Adelaide,SA&destination=Adelaide,SA&waypoints=optimize:true|Barossa+Valley,SA|Clare,SA|Connawarra,SA|McLaren+Vale,SA&key=API_KEY
+
+
+'''
+
+
 
 
 def index(request):
