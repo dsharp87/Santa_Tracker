575168c7fd0a68924660f69ba9578767047d74dd
dswayne599 <dswayne599@gmail.com>
2017-12-20 16:19:48 -0800

    models rework to include country class.  Also logic moved from views to models file. Also added country stats functionality

--- apps/city_collector_app/models.py
+++ apps/city_collector_app/models.py
@@ -5,11 +5,13 @@ from django.db import models
 import re
 import bcrypt
 import datetime
+import requests
 # Create your models here.
 
-class CityManager(models.Manager):
-    def form_validator(self, postData):
+class CountryManager(models.Manager):
+    def entry_validator(self, postData):
         errors = {}
+        #validate form input
         if len(postData['city']) < 2:
             errors["city"] = "City name must be at least 2 characters"
         if len(postData['country']) < 2:
@@ -18,35 +20,97 @@ class CityManager(models.Manager):
             print "i failed"
             return (False, errors)
         else:
-            print "i passed"
-            return (True, "yay")
-
-    def add_city(self, city, country, lat, lng):
-        print len(City.objects.filter(lat = lat).filter(lng = lng))
-        if len(City.objects.filter(lat = lat).filter(lng = lng)) > 0:
-            this_city = City.objects.filter(lat = lat).filter(lng = lng)[0]
-            this_city.entry_count += 1
-            this_city.save()
-            print this_city.entry_count
-            return this_city
-        else:
-            this_city = City.objects.create(city = city, country = country, lat = lat, lng = lng, entry_count = 1)
-            print this_city.city
-            return this_city
+            #validate form input validate google api call
+            r = requests.get('https://maps.googleapis.com/maps/api/geocode/json?address=&components=locality:' + postData['city'] + '|country:' + postData['country'] + '&key=AIzaSyAqDapqiVRbbDY1yMiVJPaJjdSRO6hlvDI').json()
+            if not r['status'] == 'OK':
+                print "i failed"
+                errors['zero_returned'] = 'We could not find your city on this planet, please try again'
+                return (False, errors)
+            else:
+                # print r['results'][0]['address_components']
+                # print r['results'][0]['geometry']
+                city = ''
+                country = ''
+                country_code = ''
+                for idx in r['results'][0]['address_components']:
+                    for key, value in idx.iteritems():
+                        if key == 'types':
+                            for spot in value:
+                                if spot == 'country':
+                                    country = idx['long_name']
+                                    country_code = idx['short_name']
+                                if spot == 'locality':
+                                    city = idx['long_name']                    
+                lat = r['results'][0]['geometry']['location']['lat']
+                lng = r['results'][0]['geometry']['location']['lng']
+                print "*"*50
+                print (city,country, country_code)
+                print (lat,lng)
+                print "*"*50
+                if city == '' or country == '':
+                    if city == '':
+                        errors['no_city'] = 'Your city was not recognized. Try larger city nearby'
+                    if country == '':
+                        errors['no_country'] = 'Your country spelling was not recognized. Try again please'
+                if len(errors):
+                    print "i failed"
+                    return (False, errors)
+                else:
+                    print "i passed"
+                    #check if country is in database
+                    if len(Country.objects.filter(name = country)) == 0:
+                        s = requests.get('http://api.worldbank.org/v2/countries/'+ country_code + '/indicators/SP.POP.DPND.YG?date=2016:2016&format=json').json()
+                        # print s
+                        percent_youth = round(s[1][0]['value'])/100
+                        print percent_youth
+                        t = requests.get('http://api.worldbank.org/v2/countries/'+ country_code + '/indicators/SL.TLF.TOTL.IN?date=2016:2016&format=json').json()
+                        # print t
+                        total_work_pop = t[1][0]['value']
+                        print total_work_pop
+                        u = requests.get('http://api.worldbank.org/v2/countries/'+ country_code + '/indicators/SP.POP.TOTL?date=2016:2016&format=json').json()
+                        # print u
+                        country_pop = u[1][0]['value']
+                        print country_pop
+                        percent_children = round(float(percent_youth*total_work_pop/country_pop), 2)
+                        print percent_children
+                        this_country = Country.objects.create(name = country, code = country_code, pop = country_pop, percent_children = percent_children)
+                        this_city = City.objects.create(city = city, country = this_country, lat = lat, lng = lng, entry_count = 1)
+                        errors['success'] = 'Your city was registered! Santa will be there soon!'
+                        return (True, errors)
+                    else:
+                        if len(City.objects.filter(lat = lat).filter(lng = lng)) > 0:
+                            this_city = City.objects.filter(lat = lat).filter(lng = lng)[0]
+                            this_city.entry_count += 1
+                            this_city.save()
+                            errors['success'] = 'Your city was registered! Santa will be there soon!'
+                            return (True, errors)
+                        else:
+                            this_country = Country.objects.filter(name = country)[0]
+                            this_city = City.objects.create(city = city, country = this_country, lat = lat, lng = lng, entry_count = 1)
+                            errors['success'] = 'Your city was registered! Santa will be there soon!'
+                            return (True, errors)
         
-
+class Country(models.Model):
+    name = models.CharField(max_length=255)
+    code = models.CharField(max_length = 3)
+    pop = models.IntegerField()
+    percent_children = models.DecimalField(max_digits=3, decimal_places=2)
+    objects = CountryManager()
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
 
 class City(models.Model):
     city = models.CharField(max_length=255)
-    country = models.CharField(max_length=255) # country needs to be its own class/table
+    country = models.ForeignKey(Country, related_name="cities")
     lat = models.FloatField()
     lng = models.FloatField()
     population = models.IntegerField(default = 0)
-    percent_children = models.FloatField(default = 0)
     entry_count = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add = True)
     updated_at = models.DateTimeField(auto_now = True)
-    objects = CityManager()
+    
+
+
 
 
 #santa actions table
--- apps/city_collector_app/views.py
+++ apps/city_collector_app/views.py
@@ -7,14 +7,17 @@ from time import gmtime, strftime
 from datetime import datetime
 import random
 import requests
+import math
 from django.contrib import messages
 from .models import *
 
 # Create your views here.
 def index(request):
     print "im indexing"
+    all_countries = Country.objects.all()
     all_cities = City.objects.all()
     context = {
+        'all_countries': all_countries,
         'all_cities': all_cities,
     }
     return render(request, 'city_collector_app/index.html', context)
@@ -29,38 +32,8 @@ def goHome(request):
 
 def process(request):
     print "im processing"
-    results = City.objects.form_validator(request.POST)
-    if results[0]:
-        r = requests.get('https://maps.googleapis.com/maps/api/geocode/json?address=&components=locality:' + request.POST['city'] + '|country:' + request.POST['country'] + '&key=AIzaSyAqDapqiVRbbDY1yMiVJPaJjdSRO6hlvDI').json()
-        if r['status'] == 'OK':
-            print r['results'][0]['address_components']
-            print r['results'][0]['geometry']
-            city = ''
-            country = ''
-            for idx in r['results'][0]['address_components']:
-                for key, value in idx.iteritems():
-                    if key == 'types':
-                        for spot in value:
-                            if spot == 'country':
-                                country = idx['long_name']
-                            if spot == 'locality':
-                                city = idx['long_name']                    
-            lat = r['results'][0]['geometry']['location']['lat']
-            lng = r['results'][0]['geometry']['location']['lng']
-            print (city,country)
-            print (lat,lng)
-            if city == '' or country == '':
-                if city == '':
-                    messages.error(request, 'Your city was not recognized. Try larger city nearby')
-                if country == '':
-                    messages.error(request, 'Your country spelling was not recognized. Try again please')
-            else:
-                City.objects.add_city(city, country, lat, lng)
-                messages.error(request, 'Your city has been registered! Santa will visit you soon!')
-        else:
-            messages.error(request, 'We could not find your city on this planet, please try again')
-    else:
-        for key, error_message in results[1].iteritems():
-            messages.error(request, error_message, extra_tags=key)
+    results = Country.objects.entry_validator(request.POST)
+    for key, error_message in results[1].iteritems():
+        messages.error(request, error_message, extra_tags=key)
 
     return redirect('/city_collector/')
